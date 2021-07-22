43fed93352a37b752371fe54b467c33bec442910
Arpita Saha <arpita.sahaa@gmail.com>
2021-07-22 18:19:53 -0400

    added styles

--- src/container/Banner.jsx
+++ src/container/Banner.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import requests from '../requests';
 import { useEffect, useState } from "react";
+import "./Banner.css"
 
 function Banner({ netflixOriginalsUrl }) {
     const [movie, setMovie] = useState([]);
@@ -15,11 +16,21 @@ function Banner({ netflixOriginalsUrl }) {
         })
     }, []);
 
+    // copy-paste from stackoverflow . when too much text on the screen, the three dots which indicates the little elipsis .
+    function truncate(str, n){
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
 
     return (
         <header className="banner" style={{ backgroundSize: "cover", backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`, backgroundPosition: "center center" }}>
             <div className="banner__contents">
-                <h1>{movie.title || movie.name}</h1>
+                <h1 className="banner_title">{movie.title || movie.name}</h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                <h1 className="banner_description">{truncate(movie.overview,100)}</h1>
+
             </div>
         </header>
     )
