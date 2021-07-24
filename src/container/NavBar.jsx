16a5292731b80b3b436c96228e49f55d1333bcd8
Arpita Saha <arpita.sahaa@gmail.com>
2021-07-24 12:05:27 -0400

    added nav css

--- src/container/NavBar.jsx
+++ src/container/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./NavBar.css";
-import logo from "../logo1.png";
+import logo from "../images/logo1.png";
+import profile from "../images/profile.png";
 
 function NavBar() {
     return (
         <div className="nav">
-            <img className="NavLogo" src={logo}/>
+            <img className="NavLogo" src={logo} alt={"MovieFlix Logo"}/>
+            <img className="profileLogo" src={profile} alt={"My Profile"}/>
         </div>
     )
 }
